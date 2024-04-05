dfec5b7c5abc619e44f7fe57c68e8b42177949f6
Sebastian Bruma <sebastian.bruma@assist.ro>
2024-04-05 15:44:50 +0300

    complete color flipper

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,18 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const changeColorBtn = document.querySelector(".btn-hero")
+changeColorBtn.addEventListener("click", changeColor)
+
+function changeColor() {
+    let selectedColor = getRandomColor()
+
+    while (selectedColor === document.body.style.backgroundColor) {
+        selectedColor = getRandomColor()
+    }
+
+    document.body.style.backgroundColor = selectedColor
+    document.querySelector(".color").textContent = selectedColor
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
